b6734a33b12d04504c9372caa949458a4a61cad6
Nikita Ershov <niershov@cloud.ru>
2025-12-04 14:12:34 +0300

    feat(PDS-3170)!: required bump version

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -4,7 +4,7 @@ export enum Themes {
 }
 
 export const TOKENS_DIRECTORY = './tokens';
-const BUILD_DIRECTORY = './build';
+export const BUILD_DIRECTORY = './build';
 export const TOKENS_BUILD_DIRECTORY = `${BUILD_DIRECTORY}/tokens`;
 export const SCSS_BUILD_DIRECTORY = `${BUILD_DIRECTORY}/scss`;
 
